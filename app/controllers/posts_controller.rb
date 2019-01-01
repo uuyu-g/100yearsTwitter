37adf2ba5cb46fcaf8633fb1c5925ec04f780bb7
yutaro <aqueous.mkli@gmail.com>
2019-01-01 18:37:31 +0900

    #11 とりあえず機能は実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,16 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all.sort_by{|post| post[:created_at].strftime('%X')}.reverse
+    posts1 = Post.created_today
+    posts2 = Post.created_yesterday
+    posts3 = Post.created_a_week_ago
+    posts4 = Post.created_a_month_ago
+    posts5 = Post.created_a_year_ago
+    @posts = posts1 + posts2 + posts3 + posts4 + posts5
+    
+    @posts = @posts.sort_by{|post| post[:created_at].strftime('%X')}.reverse
+    @posts = @posts.select do |post|
+      post[:created_at].strftime('%X') <= Time.zone.now.strftime('%X')
+    end
   end
 
   def new
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+	# 今日投稿された Post を取得
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
+ 
+  # 昨日投稿された Post を取得
+	scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
+ 
+  # 3日前に投稿された Post を取得
+  scope :created_three_days_ago, -> { where(created_at: 3.days.ago.all_day) }
+ 
+  # 一週間前に投稿された Post を取得
+  scope :created_a_week_ago, -> { where(created_at: 1.week.ago.all_day) }
+ 
+  # 一ヶ月前に投稿された Post を取得
+	scope :created_a_month_ago, -> { where(created_at: 1.month.ago.all_day) }
+
+	# 一年前に投稿された Post を取得
+  scope :created_a_year_ago, -> { where(created_at: 1.year.ago.all_day) }
+	
 end
