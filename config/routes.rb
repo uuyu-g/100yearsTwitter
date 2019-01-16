d28b9d803b2e96e8c9aa187efd33b011252d82fd
yutaro <aqueous.mkli@gmail.com>
2019-01-16 22:29:50 +0900

    ユーザーログインの機能は実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get '/' => 'home#top'
 
+  get 'login' => 'users#login_form'
+  post 'login' => 'users#login'
+  post 'logout' => 'users#logout'
+
   get 'posts/index' => 'posts#index'
   post 'posts/create' => 'posts#create'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_06_014520) do
+ActiveRecord::Schema.define(version: 2019_01_16_105145) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "tweet_image"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password"
   end
 
 end
