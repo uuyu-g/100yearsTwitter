90c2e4a0bcabfabcab18e75a0fec137876b5538d
yutaro <aqueous.mkli@gmail.com>
2019-01-17 13:05:16 +0900

    #21 bcryptをインストール

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,7 @@ class UsersController < ApplicationController
 
 	def login
 		@user = User.find_by(email: params[:email],
-												 password: params[:password]
-												 )
+												 password: params[:password])
 		if @user
 			session[:user_id] = @user.id
 			flash[:notice] = "ログインしました"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_16_105145) do
+ActiveRecord::Schema.define(version: 2019_01_17_024155) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 2019_01_16_105145) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
